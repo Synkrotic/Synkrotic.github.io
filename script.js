3ce73e774200e309e5a07a93d18042b1655dcd70
Synkrotic <121508584+Synkrotic@users.noreply.github.com>
2023-01-01 14:12:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var box = document.getElementsByClassName('searchbarbox');
+var inputBox = document.getElementsByClassName('searchbarinput');
+
+function search() {
+    var input = document.getElementById('searchbarinput').value;
+    var filePath = 'Files/Pages/' + input + '.html';
+    filePath = filePath.toLowerCase();
+
+    
+};
+
+$(document).ready(function(){
+    $('div').hover(function() {
+        if ($(this).hasClass('act-drop')){
+            $(box).addClass('animated');
+        };
+    }, function() {
+        if ($(box).hasClass('animated')){
+            $(box).removeClass('animated');
+        };
+    });
+
+    $(box).hover(function() {
+        document.onkeydown = function (e) {
+            if (e.keyCode === 13){
+                search();
+            };
+        };
+    });
+
+    $('a').hover(function() {
+        $(this).addClass('animated');
+        $('a').addClass('blurred');
+        $(this).removeClass('blurred');
+        $('li').removeClass('animated');
+    }, function() {
+        if ($(this).hasClass('animated')) {
+            $(this).removeClass('animated');
+            $('a').removeClass('blurred');
+            $('li').addClass('animated');
+        };
+    });
+});
